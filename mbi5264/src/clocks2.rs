c67638c2574248c5111582b388ff6201104bea4c
hikaricai <13061980190@163.com>
2025-01-18 01:06:50 +0800

    adding embassy

--- mbi5264/src/clocks2.rs
+++ mbi5264/src/clocks2.rs
@@ -1,8 +1,167 @@
 use embassy_rp::gpio::Level;
-use embassy_rp::peripherals::{DMA_CH0, PIO0};
+use embassy_rp::interrupt::typelevel::{Handler, Interrupt, PWM_IRQ_WRAP};
+use embassy_rp::pac::PIO0;
+use embassy_rp::peripherals::{
+    DMA_CH0, PIN_0, PIN_2, PIN_4, PIN_5, PIO0, PWM_SLICE0, PWM_SLICE1, PWM_SLICE2,
+};
 use embassy_rp::peripherals::{PIN_10, PIN_6, PIN_7, PIN_8, PIN_9};
-use embassy_rp::pio::{self, InterruptHandler, Pio, StateMachine};
+use embassy_rp::pio::{self, Instance, Pio, StateMachine};
+use embassy_rp::pwm::{self, Pwm, PwmBatch};
 use embassy_rp::{Peripheral, PeripheralRef};
+use embassy_sync::blocking_mutex::raw::CriticalSectionRawMutex;
+use embassy_sync::blocking_mutex::Mutex;
+use embassy_sync::channel::Channel;
+use embassy_sync::signal::Signal;
+
+embassy_rp::bind_interrupts!(struct PioIrqs {
+    PIO0_IRQ_0 => pio::InterruptHandler<PIO0>;
+});
+
+embassy_rp::bind_interrupts!(struct PwmIrq {
+    PWM_IRQ_WRAP => PwmInterruptHandler;
+});
+
+static PWM_CMD_SIGNAL: Signal<CriticalSectionRawMutex, PwmCmd> = Signal::new();
+static PWM_OFF_SIGNAL: Signal<CriticalSectionRawMutex, ()> = Signal::new();
+
+enum PwmCmd {
+    On,
+    Off,
+}
+struct PwmInterruptHandler {}
+
+impl Handler<PWM_IRQ_WRAP> for PwmInterruptHandler {
+    unsafe fn on_interrupt() {
+        PWM_IRQ_WRAP::unpend();
+        PWM_CMD_SIGNAL.signal(PwmCmd::Off);
+        PWM_OFF_SIGNAL.signal(());
+    }
+}
+
+pub struct LineClock {
+    pwm_gclk: Pwm<'static>,
+    pwm_a: Pwm<'static>,
+    pwm_bc: Pwm<'static>,
+    started: bool,
+    // pub running: bool,
+    // cnt: u32,
+    // pwm: rp_pico::hal::pwm::Slices,
+    // gclk_pin: Option<Pin<Gpio0, FunctionPwm, PullDown>>,
+    // gpio0_pin: Option<Pin<Gpio0, FunctionSioOutput, PullDown>>,
+    // a_pin: Pin<Gpio2, FunctionPwm, PullDown>,
+    // b_pin: Pin<Gpio4, FunctionPwm, PullDown>,
+    // c_pin: Pin<Gpio5, FunctionPwm, PullDown>,
+}
+
+impl LineClock {
+    pub fn new(
+        pwm0: PWM_SLICE0,
+        pwm1: PWM_SLICE1,
+        pwm2: PWM_SLICE2,
+        gclk_pin: PIN_0,
+        a_pin: PIN_2,
+        b_pin: PIN_4,
+        c_pin: PIN_5,
+    ) -> Self {
+        let pwm_div = 5.into();
+        PWM_IRQ_WRAP::unpend();
+        unsafe {
+            PWM_IRQ_WRAP::enable();
+        };
+        let mut gclk_cfg = pwm::Config::default();
+        gclk_cfg.divider = pwm_div;
+        gclk_cfg.top = 200 - 1;
+        gclk_cfg.compare_a = 100;
+        let pwm_gclk = Pwm::new_output_a(pwm0, gclk_pin, gclk_cfg);
+
+        let mut a_cfg = pwm::Config::default();
+        a_cfg.divider = pwm_div;
+        a_cfg.top = 100 * 64 - 1;
+        a_cfg.compare_a = 100;
+        let pwm_a = Pwm::new_output_a(pwm1, a_pin, a_cfg);
+
+        let mut bc_cfg = pwm::Config::default();
+        bc_cfg.divider = pwm_div;
+        bc_cfg.top = 100 - 1;
+        bc_cfg.compare_a = 3;
+        bc_cfg.compare_a = 1;
+        let pwm_bc = Pwm::new_output_ab(pwm2, b_pin, c_pin, bc_cfg);
+
+        Self {
+            pwm_gclk,
+            pwm_a,
+            pwm_bc,
+            started: false,
+        }
+    }
+
+    pub fn start(&mut self) {
+        self.stop();
+        PWM_OFF_SIGNAL.reset();
+        PwmBatch::set_enabled(true, |batch| {
+            batch.enable(&self.pwm_gclk);
+            batch.enable(&self.pwm_a);
+            batch.enable(&self.pwm_bc);
+        });
+        self.pwm_bc.phase_retard();
+        self.started = true;
+    }
+
+    pub fn stop(&mut self) {
+        self.started = false;
+        PwmBatch::set_enabled(false, |batch| {
+            batch.enable(&self.pwm_gclk);
+            batch.enable(&self.pwm_a);
+            batch.enable(&self.pwm_bc);
+        });
+        self.pwm_gclk.set_counter(0);
+        self.pwm_a.set_counter(0);
+        self.pwm_bc.set_counter(0);
+    }
+
+    pub async fn wait(&mut self) {
+        if !self.started {
+            return;
+        }
+        PWM_OFF_SIGNAL.wait().await;
+        self.stop();
+    }
+
+    pub async fn run_bg(mut self) {
+        loop {
+            let cmd = PWM_CMD_SIGNAL.wait().await;
+            match cmd {
+                PwmCmd::On => {
+                    self.start();
+                }
+                PwmCmd::Off => {
+                    self.stop();
+                }
+            }
+        }
+    }
+}
+
+pub struct LineClockHdl {
+    started: bool,
+}
+
+impl LineClockHdl {
+    pub fn start(&mut self) {
+        PWM_CMD_SIGNAL.signal(PwmCmd::On);
+        self.started = true;
+        PWM_OFF_SIGNAL.reset();
+    }
+
+    pub async fn wait_stop(&mut self) {
+        if !self.started {
+            return;
+        }
+        PWM_OFF_SIGNAL.wait().await;
+        self.started = false;
+    }
+}
+
 //16(chip bits) x 9(chips) + 2(empty)
 pub const CMD_BUF_SIZE: usize = 16 * 9 + 2;
 const COLOR_BUF_SIZE: usize = 16 * 9;
@@ -20,8 +179,9 @@ pub struct CmdClock {
     clk_sm: StateMachine<'static, PIO0, 0>,
     data_sm: StateMachine<'static, PIO0, 1>,
     data_ch: PeripheralRef<'static, DMA_CH0>,
-    le_prog_offset: u32,
+    le_prog_offset: u8,
     le_sm: StateMachine<'static, PIO0, 3>,
+    data_buf: [u16; CMD_BUF_SIZE],
     // le_sm_tx: rp2040_hal::pio::Tx<(PIO0, rp2040_hal::pio::SM3)>,
     // color_prog_offset: u32,
     // color_sm: rp2040_hal::pio::StateMachine<(PIO0, rp2040_hal::pio::SM2), Stopped>,
@@ -33,7 +193,8 @@ pub struct CmdClock {
 }
 
 impl CmdClock {
-    pub fn new(pio0: Pio<'static, PIO0>, pins: CmdClockPins, data_ch: DMA_CH0) -> Self {
+    pub fn new(pio0: PIO0, pins: CmdClockPins, data_ch: DMA_CH0) -> Self {
+        let pio0 = Pio::new(pio0, PioIrqs);
         let Pio {
             mut common,
             sm0: mut clk_sm,
@@ -101,7 +262,7 @@ impl CmdClock {
             ".wrap",
         );
         let le_prog = common.load_program(&le_program_data.program);
-        let le_prog_offset = le_prog.origin as u32;
+        let le_prog_offset = le_prog.origin;
         let le_pin = common.make_pio_pin(pins.le_pin);
         // le_sm.set_pins(Level::High, &[&le_pin]);
         le_sm.set_pin_dirs(pio::Direction::Out, &[&le_pin]);
@@ -116,14 +277,27 @@ impl CmdClock {
             data_ch: data_ch.into_ref(),
             le_prog_offset,
             le_sm,
+            data_buf: [0; CMD_BUF_SIZE],
         }
     }
 
-    pub async fn refresh_raw_buf(&mut self, buf: &[u16; CMD_BUF_SIZE]) {
+    pub async fn refresh(&mut self, transaction: &super::Command) {
+        let mut buf_iter = self.data_buf.iter_mut();
+        for [r, g, b] in transaction.regs {
+            for i in (0..16).rev() {
+                let buf = buf_iter.next().unwrap();
+                let r = (r >> i) & 1;
+                let g = (g >> i) & 1;
+                let b = (b >> i) & 1;
+                *buf = r + (g << 1) + (b << 2);
+                // *buf = 0x03;
+            }
+        }
+
         let data_tx = self.data_sm.tx();
         data_tx.push(ONE_COMMAND_LOOPS);
         let data_ch = self.data_ch.reborrow();
-        let dam_task = data_tx.dma_push(data_ch, buf);
+        let dma_task = data_tx.dma_push(data_ch, &self.data_buf);
 
         let le_high_cnt = 1;
         let le_low_cnt: u32 = 16 * 9 + 1 - le_high_cnt;
@@ -132,6 +306,121 @@ impl CmdClock {
         let le_data = (le_high_cnt << 16) | le_low_cnt;
         let le_tx = self.le_sm.tx();
         le_tx.push(le_data);
-        dam_task.await
+        dma_task.await;
+        self.wait_le_end();
     }
+
+    pub async fn refresh_raw_buf(&mut self, buf: &[u16; CMD_BUF_SIZE]) {
+        let data_tx = self.data_sm.tx();
+        data_tx.push(ONE_COMMAND_LOOPS);
+        let data_ch = self.data_ch.reborrow();
+        let dma_task = data_tx.dma_push(data_ch, buf);
+
+        let le_high_cnt = 1;
+        let le_low_cnt: u32 = 16 * 9 + 1 - le_high_cnt;
+        let le_low_cnt = le_low_cnt - 1;
+        let le_high_cnt = le_high_cnt - 1;
+        let le_data = (le_high_cnt << 16) | le_low_cnt;
+        let le_tx = self.le_sm.tx();
+        le_tx.push(le_data);
+        dma_task.await;
+        self.wait_le_end();
+    }
+    pub async fn refresh_empty_buf(&mut self) {
+        static BUF: [u16; 2] = [0; 2];
+        let data_tx = self.data_sm.tx();
+        data_tx.push(2 - 1);
+        let data_ch = self.data_ch.reborrow();
+        let dma_task = data_tx.dma_push(data_ch, &BUF);
+
+        let le_high_cnt = 1;
+        let le_low_cnt: u32 = 1 + 1 - le_high_cnt;
+        let le_low_cnt = le_low_cnt - 1;
+        let le_high_cnt = le_high_cnt - 1;
+        let le_data = (le_high_cnt << 16) | le_low_cnt;
+        let le_tx = self.le_sm.tx();
+        le_tx.push(le_data);
+
+        dma_task.await;
+        self.wait_le_end();
+    }
+    fn wait_le_end(&self) {
+        while self.le_end() {}
+    }
+    fn le_end(&self) -> bool {
+        let addr = embassy_rp::pac::PIO0.sm(3).addr().read().addr();
+        if addr <= self.le_prog_offset + 1 {
+            return true;
+        }
+        false
+    }
+}
+
+pub const fn gen_colors_raw_buf() -> [[u16; CMD_BUF_SIZE]; 1024] {
+    let mut data_buf: [[u16; CMD_BUF_SIZE]; 1024] = [[0; CMD_BUF_SIZE]; 1024];
+    let mut idx = 0usize;
+    let mut x = 0u16;
+    let mut y = 0u16;
+    let mut i = 0usize;
+    let mut j = 0usize;
+    loop {
+        // for y in 0..64
+        x = 0;
+        loop {
+            // for x in 0..16
+            let color = (x % 4) * 0x155;
+            // let [r, g, b] = [(y * 16 + x) << 2; 3];
+            let [r, g, b] = [color; 3];
+            let buf = &mut data_buf[idx];
+            let mut pixel_idx = 0usize;
+            j = 0;
+            loop {
+                // for j in 0..9
+                i = 15;
+                loop {
+                    // for i in (0..16).rev
+                    let r = (r >> i) & 1;
+                    let g = (g >> i) & 1;
+                    let b = (b >> i) & 1;
+                    let color = r + (g << 1) + (b << 2);
+                    buf[pixel_idx] = color;
+                    pixel_idx += 1;
+                    if i == 0 {
+                        break;
+                    }
+                    i -= 1;
+                }
+                j += 1;
+                if j >= 9 {
+                    break;
+                }
+            }
+            idx += 1;
+            x += 1;
+            if x >= 16 {
+                break;
+            }
+        }
+        y += 1;
+        if y >= 64 {
+            break;
+        }
+    }
+    return data_buf;
+}
+
+pub fn gen_raw_buf(regs: [u16; 3]) -> [u16; CMD_BUF_SIZE] {
+    let mut buf = [0; CMD_BUF_SIZE];
+    let mut buf_iter = buf.iter_mut();
+    let [r, g, b] = regs;
+    for _chip in 0..9 {
+        for i in (0..16).rev() {
+            let buf = buf_iter.next().unwrap();
+            let r = (r >> i) & 1;
+            let g = (g >> i) & 1;
+            let b = (b >> i) & 1;
+            *buf = r + (g << 1) + (b << 2);
+        }
+    }
+    buf
 }
--- mbi5264/src/main.rs
+++ mbi5264/src/main.rs
@@ -1,35 +1,12 @@
 //! Displays an animated Nyan cat
 #![no_std]
 #![no_main]
-mod clocks;
 mod clocks2;
-use core::cell::RefCell;
-use core::fmt::Write;
-
-use bsp::entry;
-use bsp::hal::{
-    clocks::{init_clocks_and_plls, Clock},
-    pac, pwm,
-    sio::Sio,
-    watchdog::Watchdog,
-};
-use clocks::{gen_raw_buf, LineClock, CMD_BUF_SIZE};
-use critical_section::Mutex;
+use clocks2::gen_raw_buf;
 use defmt::info;
-use embedded_hal::digital::{OutputPin, StatefulOutputPin};
-use rp2040_hal::gpio::{DynPinId, DynPullType, FunctionSioOutput, Pin};
-use rp2040_hal::pac::interrupt;
-use rp_pico as bsp;
-use {defmt_rtt as _, panic_probe as _};
-static GLOBAL_LINE_CLOCK: Mutex<RefCell<Option<LineClock>>> = Mutex::new(RefCell::new(None));
-type Outpin = Pin<DynPinId, FunctionSioOutput, DynPullType>;
 use embassy_executor::Spawner;
-use embassy_futures::join::join;
-use embassy_rp::bind_interrupts;
-use embassy_rp::peripherals::USB;
-use embassy_usb::driver::{Endpoint, EndpointIn, EndpointOut};
-use embassy_usb::msos::{self, windows_version};
-use embassy_usb::{Builder, Config};
+use embassy_rp::gpio;
+use {defmt_rtt as _, panic_probe as _};
 
 #[repr(packed)]
 pub struct Command {
@@ -81,185 +58,14 @@ fn gen_colors() -> [Command; 1024] {
     cmds
 }
 
-const COLOR_RAW_BUF: [[u16; clocks::CMD_BUF_SIZE]; 1024] = clocks::gen_colors_raw_buf();
+const COLOR_RAW_BUF: [[u16; clocks2::CMD_BUF_SIZE]; 1024] = clocks2::gen_colors_raw_buf();
 const UMINI_CMDS: &[(mbi5264_common::CMD, u16)] = &mbi5264_common::unimi_cmds();
-
-#[entry]
-fn entry() -> ! {
-    let mut pac = pac::Peripherals::take().unwrap();
-    let core = pac::CorePeripherals::take().unwrap();
-    let mut watchdog = Watchdog::new(pac.WATCHDOG);
-    let sio = Sio::new(pac.SIO);
-    // External high-speed crystal on the pico board is 12Mhz
-    let external_xtal_freq_hz = 12_000_000u32;
-    let clocks = init_clocks_and_plls(
-        external_xtal_freq_hz,
-        pac.XOSC,
-        pac.CLOCKS,
-        pac.PLL_SYS,
-        pac.PLL_USB,
-        &mut pac.RESETS,
-        &mut watchdog,
-    )
-    .ok()
-    .unwrap();
-
-    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
-    let pins = bsp::Pins::new(
-        pac.IO_BANK0,
-        pac.PADS_BANK0,
-        sio.gpio_bank0,
-        &mut pac.RESETS,
-    );
-    let cmd_pins = clocks::CmdClockPins {
-        clk_pin: pins.gpio6,
-        le_pin: pins.gpio7,
-        r0_pin: pins.gpio8,
-        g0_pin: pins.gpio9,
-        b0_pin: pins.gpio10,
-    };
-
-    let mut cmd_pio = clocks::CmdClock::new(cmd_pins, pac.PIO0, pac.DMA, &mut pac.RESETS);
-
-    let mut led_pin: Outpin = pins
-        .led
-        .into_push_pull_output()
-        .into_dyn_pin()
-        .into_pull_type();
-
-    let mut dbg_pin: Outpin = pins
-        .gpio11
-        .into_push_pull_output()
-        .into_dyn_pin()
-        .into_pull_type();
-
-    let pwm_slices = pwm::Slices::new(pac.PWM, &mut pac.RESETS);
-    let line_clock = LineClock::new(pwm_slices, pins.gpio0, pins.gpio2, pins.gpio4, pins.gpio5);
-    critical_section::with(move |cs| {
-        GLOBAL_LINE_CLOCK.replace(cs, Some(line_clock));
-    });
-    unsafe {
-        pac::NVIC::unmask(pac::Interrupt::PWM_IRQ_WRAP);
-    }
-    // let colors = gen_colors();
-    let sync_cmd = Command::new_sync();
-    let confirm_cmd = Command::new_confirm();
-    let mut cnt = 0;
-    let mut do_latch = false;
-    let mut raw_color_buf1: [u16; clocks::CMD_BUF_SIZE] = [0; clocks::CMD_BUF_SIZE];
-    let mut raw_color_buf2: [u16; clocks::CMD_BUF_SIZE] = [0; clocks::CMD_BUF_SIZE];
-    let mut using_raw_color_buf1 = true;
-    let mut cmd_iter = UMINI_CMDS.iter();
-    // put buf in ram, flash is tooooooo slow
-    let palette: [[u16; clocks::CMD_BUF_SIZE]; 4] = [0u16, 1, 1, 1].map(|idx| {
-        let color = idx * 0xffff;
-        gen_raw_buf([color; 3])
-    });
-    let mut frame = 0usize;
-    loop {
-        cnt += 1;
-        cmd_pio.refresh(&sync_cmd);
-        cmd_pio.commit();
-        if let Some(&(cmd, param)) = cmd_iter.next() {
-            cmd_pio.refresh(&confirm_cmd);
-            cmd_pio.commit();
-            cmd_pio.refresh(&Command::new(cmd as u8, param));
-            cmd_pio.commit();
-        }
-        // vsync
-        critical_section::with(move |cs| {
-            GLOBAL_LINE_CLOCK
-                .borrow_ref_mut(cs)
-                .as_mut()
-                .unwrap()
-                .start();
-        });
-
-        for (idx, _raw) in COLOR_RAW_BUF.iter().enumerate() {
-            // dbg_pin.set_high().unwrap();
-            let coloum_idx = idx % 16;
-            let empty_coloum_idx = (frame + 1) % 16;
-            if coloum_idx == frame {
-                cmd_pio.refresh_raw_buf(&palette[3]);
-            } else if coloum_idx == empty_coloum_idx {
-                cmd_pio.refresh_raw_buf(&palette[0]);
-            } else {
-                cmd_pio.refresh_empty_buf();
-            }
-            // match coloum_idx {
-            //     0 => {
-            //         cmd_pio.refresh_raw_buf(&palette[3]);
-            //     }
-            //     1 => {
-            //         cmd_pio.refresh_raw_buf(&palette[0]);
-            //     }
-            //     // 5..15 => {
-            //     //     // cmd_pio.refresh_raw_buf(&palette[0]);
-            //     //     cmd_pio.refresh_empty_buf();
-            //     // }
-            //     // 10 => {
-            //     //     cmd_pio.refresh_raw_buf(&palette[3]);
-            //     // }
-            //     _ => {
-            //         cmd_pio.refresh_empty_buf();
-            //     }
-            // };
-            cmd_pio.commit();
-            // dbg_pin.set_low().unwrap();
-        }
-        // cmd_pio.commit();
-        loop {
-            let sync_end = critical_section::with(move |cs| {
-                !GLOBAL_LINE_CLOCK
-                    .borrow_ref_mut(cs)
-                    .as_mut()
-                    .unwrap()
-                    .running
-            });
-            if sync_end {
-                break;
-            }
-        }
-        frame += 1;
-        frame %= 16;
-        // delay.delay_ms(1);
-        // rprintln!("mock_cmd {}", mock_cmd);
-
-        if cnt >= 10 {
-            // critical_section::with(move |cs| {
-            //     GLOBAL_LINE_CLOCK
-            //         .borrow_ref_mut(cs)
-            //         .as_mut()
-            //         .unwrap()
-            //         .start();
-            // });
-            cnt = 0;
-            led_pin.toggle().unwrap();
-        }
-    }
-}
-
-#[allow(static_mut_refs)] // See https://github.com/rust-embedded/cortex-m/pull/561
-#[interrupt]
-fn PWM_IRQ_WRAP() {
-    critical_section::with(move |cs| {
-        let mut guard = GLOBAL_LINE_CLOCK.borrow_ref_mut(cs);
-        let line_clk = guard.as_mut().unwrap();
-        line_clk.handle_interrupt();
-    });
-}
-
-use embassy_rp::peripherals::PIO0;
-use embassy_rp::pio::{InterruptHandler, Pio};
-embassy_rp::bind_interrupts!(struct PioIrqs {
-    PIO0_IRQ_0 => InterruptHandler<PIO0>;
-});
 
 #[embassy_executor::main]
 async fn main(_spawner: Spawner) {
     info!("Hello there!");
     let p = embassy_rp::init(Default::default());
-    let pio0 = Pio::new(p.PIO0, PioIrqs);
+    let mut led_pin = gpio::Output::new(p.PIN_25, gpio::Level::Low);
     let pins = clocks2::CmdClockPins {
         clk_pin: p.PIN_6,
         le_pin: p.PIN_7,
@@ -268,5 +74,55 @@ async fn main(_spawner: Spawner) {
         b0_pin: p.PIN_10,
     };
     let data_ch = p.DMA_CH0;
-    let clock2 = clocks2::CmdClock::new(pio0, pins, data_ch);
+    let mut line = clocks2::LineClock::new(
+        p.PWM_SLICE0,
+        p.PWM_SLICE1,
+        p.PWM_SLICE2,
+        p.PIN_0,
+        p.PIN_2,
+        p.PIN_4,
+        p.PIN_5,
+    );
+    let mut cmd_pio = clocks2::CmdClock::new(p.PIO0, pins, data_ch);
+
+    let sync_cmd = Command::new_sync();
+    let confirm_cmd = Command::new_confirm();
+    let mut cnt = 0;
+    let mut cmd_iter = UMINI_CMDS.iter();
+    // put buf in ram, flash is tooooooo slow
+    let palette: [[u16; clocks2::CMD_BUF_SIZE]; 4] = [0u16, 1, 1, 1].map(|idx| {
+        let color = idx * 0xffff;
+        gen_raw_buf([color; 3])
+    });
+    let mut frame = 0usize;
+    loop {
+        cnt += 1;
+        cmd_pio.refresh(&sync_cmd).await;
+        if let Some(&(cmd, param)) = cmd_iter.next() {
+            cmd_pio.refresh(&confirm_cmd).await;
+            cmd_pio.refresh(&Command::new(cmd as u8, param)).await;
+        }
+        // vsync
+        line.start();
+
+        for (idx, _raw) in COLOR_RAW_BUF.iter().enumerate() {
+            // dbg_pin.set_high().unwrap();
+            let coloum_idx = idx % 16;
+            let empty_coloum_idx = (frame + 1) % 16;
+            if coloum_idx == frame {
+                cmd_pio.refresh_raw_buf(&palette[3]).await;
+            } else if coloum_idx == empty_coloum_idx {
+                cmd_pio.refresh_raw_buf(&palette[0]).await;
+            } else {
+                cmd_pio.refresh_empty_buf().await;
+            }
+        }
+        line.wait().await;
+        frame += 1;
+        frame %= 16;
+        if cnt >= 10 {
+            cnt = 0;
+            led_pin.toggle();
+        }
+    }
 }
