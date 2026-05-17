b1c742b5c030fa8dfb4ece4f99d85abecccd7637
hikaricai <13061980190@163.com>
2026-05-17 22:06:00 +0800

    taxi 1500 fps

--- mbi5264/src/encoder.rs
+++ mbi5264/src/encoder.rs
@@ -6,6 +6,10 @@ pub struct DmaBuf {
     pub len: u32,
 }
 
+// 再大就异常了
+const RAM_IMG_SIZE: usize = 104;
+static mut IMG_RAM: [mbi5264_common::AngleImage; RAM_IMG_SIZE] =
+    [mbi5264_common::AngleImage::new(0); RAM_IMG_SIZE];
 struct EncoderCtx {
     img: &'static [mbi5264_common::AngleImage],
     idx_mod: usize,
@@ -18,10 +22,20 @@ impl EncoderCtx {
     fn new() -> Self {
         let img: &'static [mbi5264_common::AngleImage] = unsafe {
             let len = *(crate::env::IMAGE_LEN_ADDR as *const u32);
-            core::slice::from_raw_parts(
+            let img_ref: &'static [mbi5264_common::AngleImage] = core::slice::from_raw_parts(
                 crate::env::IMAGE_ADDR as *const mbi5264_common::AngleImage,
                 len as usize,
-            )
+            );
+            let img_ram = core::slice::from_raw_parts_mut(
+                IMG_RAM.as_ptr() as *mut mbi5264_common::AngleImage,
+                RAM_IMG_SIZE,
+            );
+            for (line_ram, line) in img_ram.iter_mut().zip(img_ref) {
+                rtt_target::rprintln!("iter line {}", line.angle);
+                *line_ram = *line;
+                rtt_target::rprintln!("line_ram {}", line_ram.angle);
+            }
+            img_ram
         };
         rtt_target::rprintln!("total angles {}", img.len());
         rtt_target::rprintln!("first angle {}", img[0].angle);
@@ -65,8 +79,8 @@ impl EncoderCtx {
 pub struct Encoder {
     ctx: EncoderCtx,
     buf_idx: usize,
-    buf0: [u16; 16384],
-    buf1: [u16; 16384],
+    buf0: [u16; 10000],
+    buf1: [u16; 10000],
 }
 
 impl Encoder {
@@ -74,8 +88,8 @@ impl Encoder {
         Self {
             ctx: EncoderCtx::new(),
             buf_idx: 0,
-            buf0: [0; 16384],
-            buf1: [0; 16384],
+            buf0: [0; 10000],
+            buf1: [0; 10000],
         }
     }
     pub fn encode_next(&mut self, angle: u32) -> Option<DmaBuf> {
